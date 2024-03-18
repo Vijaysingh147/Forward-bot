e21da18cdec566504788be3223cdabe4093c984b
Locotocopocochoco <138638298+Locotocopocochoco@users.noreply.github.com>
2024-03-18 07:36:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "1a28639a79e913d356bf202e1612fcda")
     BOT_TOKEN = environ.get("BOT_TOKEN", "6344589382:AAFKDjHeFA8eKRhZPv1uoE7jNwGeQHTIm0E")
     BOT_SESSION = environ.get("BOT_SESSION", "bot")
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Merlebot:omNahXZhkYMP4dNf@cluster787.ssmfokm.mo>
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Merlebot:omNahXZhkYMP4dNf@cluster787.ssmfokm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster787"
     DATABASE_NAME = environ.get("DATABASE_NAME", "Merlebot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1436817644').split()]
 
