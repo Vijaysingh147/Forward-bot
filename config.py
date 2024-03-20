85471b27589f18549ea6a53c54b105a58414f8b7
Locotocopocochoco <138638298+Locotocopocochoco@users.noreply.github.com>
2024-03-20 22:29:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
 class Config:
-    API_ID = environ.get("API_ID", "28249550")
-    API_HASH = environ.get("API_HASH", "1a28639a79e913d356bf202e1612fcda")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6344589382:AAFKDjHeFA8eKRhZPv1uoE7jNwGeQHTIm0E")
+    API_ID = environ.get("API_ID", "")
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "")
     BOT_SESSION = environ.get("BOT_SESSION", "bot")
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Merlebot:omNahXZhkYMP4dNf@cluster787.ssmfokm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster787")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Merlebot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1436817644').split()]
+    DATABASE_URI = environ.get("DATABASE", "")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
 
 class temp(object):
     lock = {}
